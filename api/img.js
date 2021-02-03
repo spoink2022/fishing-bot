dbc687bbc9a8ec7b652482d362cc6305204876a6
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2021-02-03 11:19:29 -0500

    added trash images

--- api/img.js
+++ api/img.js
@@ -1,3 +1,7 @@
 module.exports.fetchLocationImgUrl = function(locationID) {
     return `https://raw.githubusercontent.com/spoink2022/fishing-bot/main/static/img/unused/${locationID}_bg.jpg`
 }
+
+module.exports.fetchFishImgUrl = function(fishID, imgNum) {
+    return `https://raw.githubusercontent.com/spoink2022/fishing-bot/main/static/img/fish/${fishID}_${imgNum}.png`;
+}
