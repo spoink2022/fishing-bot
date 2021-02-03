95d516eddafd4208a927f7da94f1a31be298163f
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2021-02-03 14:04:59 -0500

    added hook img

--- api/fishing.js
+++ api/fishing.js
@@ -1,5 +1,6 @@
 const FishingLocationsData = require('./data/fishing-locations.json');
 const FishData = require('./data/fish.json');
+const EquipmentData = require('./data/equipment.json');
 
 
 module.exports.getLocationData = function(id) {
@@ -8,4 +9,12 @@ module.exports.getLocationData = function(id) {
 
 module.exports.getFishData = function(id) {
     return FishData[id-1];
+}
+
+module.exports.getRodData = function(id) {
+    return EquipmentData.rod[id];
+}
+
+module.exports.getLineData = function(id) {
+    return EquipmentData.line[id];
 }
