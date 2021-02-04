405575684a18186d2e8ded09592b9c96b3c81ed9
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2021-02-04 17:08:31 -0500

    cache images in memory

--- db/config.js
+++ db/config.js
@@ -13,6 +13,6 @@ module.exports.pquery = async function(text, params) {
     const start = Date.now();
     const result = await pool.query(text, params);
     const duration = Date.now() - start;
-    console.log('executed query', {text, duration, rows: result.rowCount});
+    //console.log('executed query', {text, duration, rows: result.rowCount});
     return result.rows;
 };
