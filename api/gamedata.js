1ad210284c5ff7a5436743340ab238157443b3d6
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2021-02-08 16:28:29 -0500

    image remake

--- api/gamedata.js
+++ api/gamedata.js
@@ -3,3 +3,11 @@ const AquariumData = require('./data/aquarium.json');
 module.exports.getAquariumInfo = function(id) {
     return AquariumData[id.toString()];
 }
+
+module.exports.getAquariumLabels = function() {
+    let labels =  {};
+    for(const val of Object.values(AquariumData)) {
+        labels[val.name] = val.label;
+    }
+    return labels;
+}
