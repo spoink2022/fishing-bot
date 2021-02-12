f027eee27d0cfe9d5f951e7a22fea0d2b80d1700
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2021-02-12 16:46:38 -0500

    location 2 images

--- api/visuals.js
+++ api/visuals.js
@@ -1,5 +1,5 @@
 const ColorData = require('./data/colors.json');
 
-module.exports.getColor = function(colourName) {
+module.exports.getColor = function(colorName) {
     return ColorData[colorName];
 }
