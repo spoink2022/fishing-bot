a3add1ff8f7ed8377639f9e0fe7653201a59c287
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2021-02-14 15:57:40 -0500

    finished info command

--- api/text.js
+++ api/text.js
@@ -3,3 +3,6 @@ const TextData = require('./data/text.json');
 module.exports.getText = function(category, key) {
     return TextData[category][key];
 }
+module.exports.getTitles = function(category) {
+    return Object.keys(TextData[category]);
+}
