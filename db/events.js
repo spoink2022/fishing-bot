9c1a423ef6526d4e686f7d9fae90d7781830d8b9
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2021-02-27 14:46:50 -0500

    event settings infrastructure

--- db/events.js
+++ db/events.js
@@ -1,1 +1,19 @@
 const config = require('./config.js');
+console.log(Date.now());
+console.log(Date.now() + 1*24*60*60*1000);
+console.log(Date.now() + 3*24*60*60*1000);
+module.exports.setEvent = async function(eventInput) {
+    let query = `INSERT INTO events (type, params, start_time, end_time, description, create_time) VALUES ($1, $2, $3, $4, $5, $6)`;
+    await config.pquery(query, Object.values(eventInput));
+    return;
+}
+
+module.exports.getUpcomingEvent = async function() {
+    let query = 'SELECT * FROM events WHERE end_time > $1 ORDER BY create_time DESC LIMIT 1'; // grabs the latest event entry that hasnt passed yet
+    let eventEntry = (await config.pquery(query, [Date.now()]))[0];
+    if(eventEntry) {
+        eventEntry.start_time = parseInt(eventEntry.start_time);
+        eventEntry.end_time = parseInt(eventEntry.end_time);
+    }
+    return eventEntry;
+}
--- db/index.js
+++ db/index.js
@@ -1,3 +1,4 @@
 module.exports.users = require('./users.js');
 module.exports.aquarium = require('./aquarium.js');
 module.exports.stats = require('./stats.js');
+module.exports.events = require('./events.js');
