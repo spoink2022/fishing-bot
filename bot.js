2f31de1a400b2ec418d0ef4ea478180aef9d1b91
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2021-03-11 11:01:52 -0500

    autoposting

--- bot.js
+++ bot.js
@@ -1,4 +1,7 @@
+const auth = require('./static/private/auth.json');
+
 global.PREFIX = '.';
 global.client = require('./lib/client.js');
 require('./lib/server.js');
+if(auth.topggToken) { require('./lib/autopost.js'); }
 require('./lib/global/load_images.js');
