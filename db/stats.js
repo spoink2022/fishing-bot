ae3b09f6eeb832806a34e9e301f0712ebd50c76f
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2021-04-01 17:29:08 -0400

    serverstats

--- db/stats.js
+++ db/stats.js
@@ -1,5 +1,11 @@
 const config = require('./config.js');
 
+async function createServerEntry(serverid) {
+    let query = 'INSERT INTO servers (serverid) VALUES ($1)';
+    await config.pquery(query, [serverid]);
+    return;
+}
+
 module.exports.fetchStats = async function(userid) {
     let query = 'SELECT * FROM stats WHERE userid=$1';
     let stats = (await config.pquery(query, [userid]))[0];
@@ -17,5 +23,31 @@ module.exports.integrateCatch = async function(userid, fishWeight) {
 module.exports.fetchWeightCaughtForLeaderboards = async function(useridArray) {
     let query = 'SELECT userid, weight_caught FROM stats WHERE userid=ANY($1)';
     let res = await config.pquery(query, [useridArray]);
+    return res;
+}
+
+module.exports.integrateCatchServer = async function(serverid, fishWeight) {
+    let grams = Math.round(fishWeight*1000);
+    let query = 'UPDATE servers SET fish_caught=fish_caught+1, weight_caught=weight_caught+$1 WHERE serverid=$2 RETURNING serverid';
+    let res = await config.pquery(query, [grams, serverid]);
+    if (!res[0]) {
+        await createServerEntry(serverid);
+        await config.pquery(query, [grams, serverid]);
+    }
+    return;
+}
+
+module.exports.fetchServerStats = async function(serverid) {
+    let query = 'SELECT * FROM servers WHERE serverid=$1';
+    let res = (await config.pquery(query, [serverid]))[0];
+    if (!res) {
+        await createServerEntry(serverid);
+        res = { fish_caught: 0, weight_caught: 0 };
+    }
+    query = 'SELECT position FROM (SELECT serverid, RANK() OVER(ORDER BY weight_caught DESC) AS position FROM servers) RESULT WHERE serverid=$1';
+    let rank = (await config.pquery(query, [serverid]))[0].position;
+    res.fish_caught = parseInt(res.fish_caught);
+    res.weight_caught = parseInt(res.weight_caught);
+    res.rank = parseInt(rank);
     return res;
 }
