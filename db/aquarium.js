c283a89efbd9c7a214b2ead5f091ee6955c9cc1b
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2021-08-27 17:06:43 -0400

    fishleaderboards

--- db/aquarium.js
+++ db/aquarium.js
@@ -19,3 +19,24 @@ module.exports.getMultiplierValues = async function(userid, fishColumns) {
     let multiplierValues = (await config.pquery(query, [userid]))[0];
     return multiplierValues;
 }
+
+module.exports.fetchSpeciesForLeaderboards = async function(useridArray, speciesName) {
+    let columnName = speciesName.replace(/ /g, '_');
+    let query = `SELECT userid, ${columnName} FROM aquarium WHERE userid=ANY($1) AND ${columnName} IS NOT NULL`;
+    let res = await config.pquery(query, [useridArray]);
+    return res;
+}
+
+module.exports.fetchSpeciesForGlobalLeaderboards = async function(speciesName) {
+    let columnName = speciesName.replace(/ /g, '_');
+    let query = `SELECT userid, ${columnName} FROM aquarium WHERE ${columnName} IS NOT NULL ORDER BY ${columnName} DESC LIMIT 10`;
+    let res = await config.pquery(query, []);
+    return res;
+}
+
+module.exports.fetchSpeciesUserRanking = async function(userid, speciesName) {
+    let columnName = speciesName.replace(/ /g, '_');
+    let query = `WITH LIST AS (SELECT userid, ${columnName}, RANK() OVER(ORDER BY ${columnName} DESC) rank FROM aquarium WHERE ${columnName} IS NOT NULL) SELECT rank, ${columnName} FROM list WHERE userid=$1`;
+    let res = await config.pquery(query, [userid]);
+    return res[0];
+}
