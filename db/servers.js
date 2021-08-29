c124cb575e1777e6507bc7fa5b09230b4b13bfa0
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2021-08-28 20:35:32 -0400

    setprefix functionality

--- db/servers.js
+++ db/servers.js
@@ -1,19 +1,18 @@
 const config = require('./config.js');
 
 async function createServerEntry(serverid) {
-    let query = 'INSERT INTO servers (serverid) VALUES ($1)';
+    let query = 'INSERT INTO servers (serverid) VALUES ($1) RETURNING *';
     await config.pquery(query, [serverid]);
     return;
 }
 
 async function fetchServer(serverid) {
     let query = 'SELECT * FROM servers WHERE serverid=$1';
-    let res = (await config.pquery(query, [serverid]))[0];
+    let res = await config.pquery(query, [serverid]);
     if (!res) {
-        await createServerEntry(serverid);
-        res = { promote: false };
+        res = await createServerEntry(serverid);
     }
-    return res;
+    return res[0];
 }
 
 module.exports.fetchServer = fetchServer;
@@ -23,4 +22,11 @@ module.exports.toggle = async function(serverid, column) {
     let query = `UPDATE servers SET ${column} = NOT ${column} WHERE serverid=$1 RETURNING ${column}`;
     let res = (await config.pquery(query, [serverid]))[0];
     return res;
+}
+
+module.exports.setPrefix = async function(serverid, prefix) {
+    await fetchServer(serverid);
+    let query = 'UPDATE servers SET prefix=$1 WHERE serverid=$2';
+    await config.pquery(query, [prefix, serverid]);
+    return;
 }
