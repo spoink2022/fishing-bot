56a26d0410768a24b74ea5fa34fe3d40dbf16390
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2021-10-17 14:07:53 -0400

    .skin command functionality

--- api/cosmetics.js
+++ api/cosmetics.js
@@ -3,3 +3,11 @@ let CosmeticData = require('./data/cosmetics.json');
 module.exports.getAllEquipmentSkins = function() {
     return CosmeticData.equipment_banner;
 }
+
+module.exports.getAllCosmeticData = function() {
+    return CosmeticData;
+}
+
+module.exports.getCategoryData = function(category) {
+    return CosmeticData[category];
+}
--- db/cosmetics.js
+++ db/cosmetics.js
@@ -5,3 +5,44 @@ module.exports.getEquippedCosmetic = async function(userid, category) {
     let res = await config.pquery(query, [category, userid]);
     return res[0] ? res[0].cosmetic_id : null;
 }
+
+module.exports.getAllCosmetics = async function(userid) {
+    let query = 'SELECT * FROM cosmetics WHERE userid=$1 ORDER BY id';
+    let res = await config.pquery(query, [userid]);
+    return res;
+}
+
+module.exports.getCosmetic = async function(userid, category, index) {
+    let query = 'SELECT * FROM cosmetics WHERE userid=$1 AND category=$2 ORDER BY id';
+    let res = await config.pquery(query, [userid, category]);
+    return res[index];
+}
+
+module.exports.getCosmeticCount = async function(category, cosmeticID) {
+    let query = 'SELECT COUNT(id) FROM cosmetics WHERE category=$1 AND cosmetic_id=$2';
+    let res = await config.pquery(query, [category, cosmeticID]);
+    return res[0].count;
+}
+
+// MODIFY COSMETICS ENTRY
+module.exports.unequipCosmetic = async function(id) {
+    let query = 'UPDATE cosmetics SET equipped=FALSE WHERE id=$1';
+    await config.pquery(query, [id]);
+    return;
+}
+
+module.exports.equipCosmetic = async function(id, category) {
+    // set other equips in category to false
+    let query = 'UPDATE cosmetics SET equipped=FALSE WHERE category=$1 AND equipped=TRUE AND NOT id=$2';
+    await config.pquery(query, [category, id]);
+
+    query = 'UPDATE cosmetics SET equipped=TRUE WHERE id=$1';
+    config.pquery(query, [id]);
+    return;
+}
+
+module.exports.deleteCosmetic = async function(id) {
+    let query = 'DELETE FROM cosmetics WHERE id=$1';
+    await config.pquery(query, [id]);
+    return;
+}
