75b55d32629aa329723a96a7d54f02134a5b673d
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2021-12-25 19:03:57 -0500

    canvas for equipment command

--- db/cosmetics.js
+++ db/cosmetics.js
@@ -1,7 +1,7 @@
 const config = require('./config.js');
 
 module.exports.getEquippedCosmetic = async function(userid, category) {
-    let query = `SELECT cosmetic_id FROM cosmetics WHERE category=$1 AND equipped=TRUE AND userid=$2`;
+    let query = `SELECT cosmetic_id FROM cosmetics WHERE category=$1 AND equipped=TRUE AND userid=$2 ORDER BY id LIMIT 1`;
     let res = await config.pquery(query, [category, userid]);
     return res[0] ? res[0].cosmetic_id : null;
 }
