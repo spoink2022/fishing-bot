9e6e373852639a1e83af3f5f4f2d7affa3a2d16a
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2021-12-28 13:53:45 -0500

    buy ring

--- api/equipment.js
+++ api/equipment.js
@@ -4,6 +4,22 @@
 const EquipmentData = require('./data/equipment.json');
 const RingData = require('./data/rings.json');
 
+// Formatted Data
+RingData.packChances.regularPrefixSum = getPrefixSum(RingData.packChances.regular);
+RingData.packChances.premiumPrefixSum = getPrefixSum(RingData.packChances.premium);
+const RingNames = Object.keys(RingData.types);
+
+// Formatting Functions
+function getPrefixSum(arr) {
+    let prefixSum = [];
+    for (let i=0; i<arr.length; i++) {
+        prefixSum.push(Math.round((arr[i] + (prefixSum[i-1] || 0)) * 10 ) / 10);
+    }
+    return prefixSum;
+}
+
+// Exports
+
 module.exports.getRodData = function(id) {
     return EquipmentData.rods[id];
 }
@@ -31,4 +47,17 @@ module.exports.getSwivelData = function(id) {
 // Rings
 module.exports.getRingPackData = function(userLevel) {
     return RingData.packs.find(element => userLevel < element.maxLevel);
+}
+
+module.exports.getRingSellRate = function(rating) {
+    return RingData.sellRates[rating];
+}
+
+// Rings - Bulk
+module.exports.getRingNames = function(ringNames) {
+    return RingNames;
+}
+
+module.exports.getRingPackChances = function(packType) {
+    return RingData.packChances[`${packType}PrefixSum`];
 }
--- db/rings.js
+++ db/rings.js
@@ -5,3 +5,15 @@ module.exports.getRing = async function(ringID) {
     let ring = (await config.pquery(query, [ringID]))[0];
     return ring;
 }
+
+module.exports.getAllUserRings = async function(userid) {
+    let query = 'SELECT * FROM rings WHERE userid=$1 ORDER BY id';
+    let rings = await config.pquery(query, [userid]);
+    return rings;
+}
+
+module.exports.addRing = async function(r) {
+    let query = 'INSERT INTO rings (userid, ring_type, value, s, m, l, xl, premium, sashimi, trophy) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING id';
+    let ringID = (await config.pquery(query, [r.userid, r.ring_type, r.value, r.s, r.m, r.l, r.xl, r.premium, r.sashimi, r.trophy]))[0].id;
+    return ringID;
+}
