939b4cf1e0e6f85c99a097cdaae4a82ab77bf61f
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2021-12-30 12:00:21 -0500

    clan promote command

--- db/clan.js
+++ db/clan.js
@@ -15,9 +15,9 @@ module.exports.createClan = async function(name) {
     return clanId;
 }
 
-module.exports.createClanMember = async function(userid, tag, clanId, role=0) {
-    let query = 'INSERT INTO clan_member (userid, tag, clan, role, joined) VALUES ($1, $2, $3, $4, $5)';
-    return await config.pquery(query, [userid, tag, clanId, role, Date.now()]);
+module.exports.setClanName = async function(clanId, newName, paid=true) {
+    let query = `UPDATE clan SET name=$1, rename=rename-$2 WHERE id=$3`;
+    return await config.pquery(query, [newName, paid ? 1 : 0, clanId]);
 }
 
 module.exports.fetchMember = async function(userid) {
@@ -31,15 +31,26 @@ module.exports.setMemberTag = async function(userid, username) {
     return await config.pquery(query, [username, userid]);
 }
 
-module.exports.fetchMembers = async function(clanId) {
-    let query = 'SELECT users.opted_in, users.level, users.cooldown, clan_member.joined, clan_member.userid, clan_member.tag, clan_member.role, clan_member.campaign_catches, clan_member.last_campaign_catch FROM users, clan_member WHERE users.userid=clan_member.userid AND users.clan=$1 ORDER BY users.level DESC';
-    let res = await config.pquery(query, [clanId]);
+module.exports.setMemberRole = async function(userid, newRole) {
+    let query = 'UPDATE clan_member SET role=$1 WHERE userid=$2';
+    return await config.pquery(query, [newRole, userid]);
+}
+
+module.exports.createClanMember = async function(userid, tag, clanId, role=0) {
+    let query = 'INSERT INTO clan_member (userid, tag, clan, role, joined) VALUES ($1, $2, $3, $4, $5)';
+    return await config.pquery(query, [userid, tag, clanId, role, Date.now()]);
+}
+
+module.exports.fetchMemberByMemberId = async function(clanId, memberId) { // memberId is index in memberlist
+    let query = 'SELECT clan_member.* FROM users, clan_member WHERE users.userid=clan_member.userid AND users.clan=$1 ORDER BY users.level DESC, users.exp DESC OFFSET $2 LIMIT 1';
+    let res = (await config.pquery(query, [clanId, memberId-1]))[0];
     return res;
 }
 
-module.exports.setClanName = async function(clanId, newName, paid=true) {
-    let query = `UPDATE clan SET name=$1, rename=rename-$2 WHERE id=$3`;
-    return await config.pquery(query, [newName, paid ? 1 : 0, clanId]);
+module.exports.fetchMembers = async function(clanId) {
+    let query = 'SELECT users.opted_in, users.level, users.cooldown, clan_member.joined, clan_member.userid, clan_member.tag, clan_member.role, clan_member.campaign_catches, clan_member.last_campaign_catch FROM users, clan_member WHERE users.userid=clan_member.userid AND users.clan=$1 ORDER BY users.level DESC, users.exp DESC';
+    let res = await config.pquery(query, [clanId]);
+    return res;
 }
 // NEW -- END
 
