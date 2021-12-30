05786accfbbf0aa2469253650bdd0752dbfd4671
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2021-12-30 18:18:58 -0500

    clan shop command

--- api/clan.js
+++ api/clan.js
@@ -6,3 +6,7 @@ const ClanShopData = require('./data/clanshop.json');
 module.exports.getPerkValue = function(name, level) {
     return ClanShopData.perks[name].levels[level-1].value;
 }
+
+module.exports.getAllClanShopData = function() {
+    return ClanShopData;
+}
