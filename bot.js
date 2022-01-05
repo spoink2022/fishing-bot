57eaa157f17b32f06f2471d010f17e67a6a383f6
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2022-01-05 14:13:08 -0500

    top.gg autoposting

--- bot.js
+++ bot.js
@@ -1,15 +1,5 @@
-// Run all tasks necessary to Big Tuna
-// # ------------------------------- #
-const { topggToken } = require('./private/config.json');
-
 // login client
 global.client = require('./lib/client.js');
 
-// autopost stats to top.gg (Production only)
-if (topggToken) { require('./lib/autopost.js'); }
-
 // preload images
 require('./lib/global/load_images.js');
-
-// register slash commands
-require('./lib/deploy-commands.js');
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Shard Manager
 const { ShardingManager } = require('discord.js');
+const { AutoPoster } = require('topgg-autoposter');
 
-const { discordToken } = require('./private/config.json');
+const { discordToken, topggToken } = require('./private/config.json');
 
 const manager = new ShardingManager('./bot.js', { totalShards: 2, token: discordToken });
 
@@ -11,3 +12,15 @@ manager.spawn();
 
 // spin-up webserver for top.gg votes
 require('./lib/server.js');
+
+// register slash commands
+require('./lib/deploy-commands.js');
+
+// autopost stats to top.gg (Production only)
+if (topggToken) {
+    const ap = AutoPoster(topggToken, manager);
+
+    ap.on('posted', () => {
+        console.log('Posted stats to top.gg');
+    });
+}
