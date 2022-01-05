efd92276e1245779434b8527f5c964c6b6064548
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2022-01-05 15:06:36 -0500

    shortcuts

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { AutoPoster } = require('topgg-autoposter');
 
 const { discordToken, topggToken } = require('./private/config.json');
 
-const manager = new ShardingManager('./bot.js', { totalShards: 2, token: discordToken });
+const manager = new ShardingManager('./bot.js', { token: discordToken });
 
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 
