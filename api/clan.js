7d7b8854f6f157a7ff49f2dd97880ee901ca50aa
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2022-02-10 21:35:20 -0500

    clan boat parts (partial)

--- api/clan.js
+++ api/clan.js
@@ -2,6 +2,7 @@
 // # ------=----------- #
 
 const ClanShopData = require('./data/clanshop.json');
+const ClanBoatData = require('./data/clanboat.json');
 
 module.exports.getPerkValue = function(name, level) {
     return ClanShopData.perks[name].levels[level-1].value;
@@ -13,4 +14,45 @@ module.exports.getPerkData = function(name) {
 
 module.exports.getAllClanShopData = function() {
     return ClanShopData;
+}
+
+// CLAN BOAT
+module.exports.getHullCount = function() {
+    return ClanBoatData.hull.length;
+}
+module.exports.getAllHullData = function() {
+    return ClanBoatData.hull;
+}
+module.exports.getHullData = function(id) {
+    return ClanBoatData.hull[id - 1];
+}
+
+module.exports.getEngineCount = function() {
+    return ClanBoatData.engine.length;
+}
+module.exports.getAllEngineData = function() {
+    return ClanBoatData.engine;
+}
+module.exports.getEngineData = function(id) {
+    return ClanBoatData.engine[id - 1];
+}
+
+module.exports.getContainerCount = function() {
+    return ClanBoatData.container.length;
+}
+module.exports.getAllContainerData = function() {
+    return ClanBoatData.container;
+}
+module.exports.getContainerData = function(id) {
+    return ClanBoatData.container[id - 1];
+}
+
+module.exports.getPropellerCount = function() {
+    return ClanBoatData.propeller.length;
+}
+module.exports.getAllPropellerData = function() {
+    return ClanBoatData.propeller;
+}
+module.exports.getPropellerData = function(id) {
+    return ClanBoatData.propeller[id - 1];
 }
--- api/img.js
+++ api/img.js
@@ -51,3 +51,20 @@ module.exports.fetchRingImgUrl = function(ringName) {
 module.exports.fetchSwivelImgUrl = function(swivelID) {
     return `${path}/equipment/swivels/swivel_${swivelID}.png`;
 }
+
+// clan boat
+module.exports.fetchHullImgUrl = function(hullId) {
+    return `${path}/clanboat/hull/hull_${hullId}.png`;
+}
+
+module.exports.fetchEngineImgUrl = function(engineId) {
+    return `${path}/clanboat/engine/engine_${engineId}.png`;
+}
+
+module.exports.fetchContainerImgUrl = function(containerId) {
+    return `${path}/clanboat/container/container_${containerId}.png`;
+}
+
+module.exports.fetchPropellerImgUrl = function(propellerId) {
+    return `${path}/clanboat/propeller/propeller_${propellerId}.png`;
+}
