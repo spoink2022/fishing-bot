7bb70bb85c31b29b54b85b7b7541d4df463cbb0f
Nathan Chin <spoink2022@gmail.com>
2022-09-26 21:48:44 -0400

    clan boat update

--- scripts/recalculate_scores.js
+++ scripts/recalculate_scores.js
@@ -6,14 +6,14 @@ const { calculateFisherScore } = require('../lib/misc/calculations.js');
 async function recalculate() {
     const AllAquariumData = await db.aquarium.getAllData();
     const LocationFishData = [];
-    for (let i=1; i<=11; i++) {
+    for (let i=1; i<=13; i++) {
         LocationFishData.push(api.fish.getFishDataFromLocation(i));
     }
     let increment = 0;
     for (let entry of AllAquariumData) {
         increment++;
         let scores = [];
-        for (let i=0; i<11; i++) {
+        for (let i=0; i<13; i++) {
             scores.push(calculateFisherScore(entry, LocationFishData[i]));
         }
         console.log(increment);
